bea5aa1c558a8da51360e109f90a102f1a9c8e88
Muthukrish0682 <138483379+Muthukrish0682@users.noreply.github.com>
2023-07-05 10:12:55 +0530

    Add files via upload

---
+++ Final_conv_file_v1_web.py
@@ -0,0 +1,63 @@
+import pandas as pd
+from fractions import Fraction
+import streamlit as st
+import base64
+import io
+
+def to_excel(df):
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, sheet_name='Sheet1', index=False)
+    output.seek(0)
+    return output.read()
+
+def get_table_download_link(df):
+    val = to_excel(df)
+    b64 = base64.b64encode(val)
+    return f'<a href="data:application/octet-stream;base64,{b64.decode()}" download="output.xlsx" class="download_link">Download Excel file</a>'
+
+def main():
+    st.title(" 👋 Decimal to Fraction Converter")
+    st.write("Version 2.0")
+
+    # User selects the rounding denominator
+    rounding_denominator = st.selectbox('Select rounding denominator', [8, 16, 32], index=1)
+
+    # Upload the dataset
+    file = st.file_uploader("Upload an excel file", type=['xlsx'])
+
+    if file is not None:
+        # Read the file
+        df = pd.read_excel(file)
+
+        # Assuming the column with the decimals is named 'Decimals'
+        fractions = []
+        for decimal in df['Decimals']:
+            # Round to the nearest 1/rounding_denominator
+            rounded_decimal = round(decimal * rounding_denominator) / rounding_denominator
+
+            # Convert decimal to fraction
+            fraction = Fraction(rounded_decimal).limit_denominator()
+
+            # Convert the fraction to a string
+            if fraction.numerator > fraction.denominator:
+                whole_num = fraction.numerator // fraction.denominator
+                numerator = fraction.numerator - whole_num * fraction.denominator
+                fraction_str = f"{whole_num} {numerator}/{fraction.denominator}\"" if numerator != 0 else f"{whole_num}\""
+            else:
+                fraction_str = f"{fraction}\""
+
+            fractions.append(fraction_str)
+
+        # Add Fractions column to the existing dataframe
+        df['Fractions'] = fractions
+
+        # Display the dataframe
+        st.dataframe(df)
+
+        # Provide download link for the dataframe in Excel format
+        if st.button('Download Excel file'):
+            st.markdown(get_table_download_link(df), unsafe_allow_html=True)
+
+if __name__ == '__main__':
+    main()
